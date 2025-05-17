e694e7ad2df92f89c2f320801bf6f7ecbd48c53f
TathilaWijemanne <kethmikaw@gmail.com>
2025-05-17 14:52:54 +0530

    BAsic Version

---
+++ Ultrasonic_Main/build/release/Ultrasonic_Main.d
@@ -0,0 +1,6 @@
+build/release/Ultrasonic_Main.o: Ultrasonic_Main.cpp \
+ C:\Users\kethm\AppData\Local\Programs\Webots/include/controller/cpp/webots/DistanceSensor.hpp \
+ C:\Users\kethm\AppData\Local\Programs\Webots/include/controller/cpp/webots/Device.hpp \
+ C:/Users/kethm/AppData/Local/Programs/Webots/include/controller/c/webots/types.h \
+ C:\Users\kethm\AppData\Local\Programs\Webots/include/controller/cpp/webots/Motor.hpp \
+ C:\Users\kethm\AppData\Local\Programs\Webots/include/controller/cpp/webots/Robot.hpp
